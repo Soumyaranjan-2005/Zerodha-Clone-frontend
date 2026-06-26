7cc0e757b0c67c7a3b6c60787d6edfb84adf66bd
Soumya Ranjan Mahanna <soumyaranjanmahanna5@gmail.com>
2026-06-26 16:16:31 +0530

    Delete backend directory

--- backend/model/OrdersModel.js
+++
@@ -1,7 +0,0 @@
-const { model } = require('mongoose');
-
-const { OrdersSchema } = require('../schemas/OrdersSchema');
-
-const OrdersModel = model('order', OrdersSchema);
-
-module.exports = { OrdersModel };
