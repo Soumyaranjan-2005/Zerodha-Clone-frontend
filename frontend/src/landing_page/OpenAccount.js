87bea13d3eddf915c056ab1c3ec8436fdc26aa64
Soumya Ranjan Mahanna <soumyaranjanmahanna5@gmail.com>
2026-06-26 14:40:00 +0530

    implement responsiveness 15

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -1,22 +1,14 @@
-import React from 'react';
-
-function OpenAccount() {
-  return ( 
-<<<<<<< HEAD
-        <div className='container p-5 mb-5'>
-        <div className='row text-center'>
-           <h1 className='mt-5 fs-3'>Open a Zerodha account</h1>
-           <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-           <button className='p-2 btn btn-primary fs-5 mb-5 w-75 w-md-50 w-lg-25' style={{margin: "0 auto",}}>Signup Now</button>
-=======
-    <div className='container p-5 mb-5'>
-        <div className='row text-center'>
-           <h1 className='mt-5 fs-3'>Open a Zerodha account</h1>
-           <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-           <button className='p-2 btn btn-primary fs-5 mb-5' style={{width: "80%", maxWidth: "250px", margin: "0 auto",}}>Signup Now</button>
->>>>>>> def57996 (Make frontend and dashboard mobile responsive)
-        </div>
-    </div>
-     );
-}
-export default OpenAccount;
+import React from 'react';
+
+function OpenAccount() {
+  return ( 
+    <div className='container p-5 mb-5'>
+        <div className='row text-center'>
+           <h1 className='mt-5 fs-3'>Open a Zerodha account</h1>
+           <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+           <button className='p-2 btn btn-primary fs-5 mb-5' style={{width: "80%", maxWidth: "250px", margin: "0 auto"}}>Signup Now</button>
+        </div>
+    </div>
+     );
+}
+export default OpenAccount;
--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -3,19 +3,11 @@ import React from 'react';
 function Education() {
   return ( 
     <div className='container mt-5 mb-4'>
-<<<<<<< HEAD
         <div className='row align-items-center'>
-          <div className='col-12 col-lg-6 mb-4 mb-lg-0'>
-            <img src='\media\images\education.svg' alt="Education illustration" style={{width:"70%"}} className='img-fluid'/>
-          </div>
-          <div className='col-12 col-lg-6 mt-5'>
-=======
-        <div className='row'>
-          <div className='col-12 col-md-6'>
+          <div className='col-12 col-md-6 mb-4 mb-md-0'>
             <img src='\media\images\education.svg' alt="Education illustration" style={{width:"70%"}} className='img-fluid'/>
           </div>
           <div className='col-12 col-md-6 mt-5'>
->>>>>>> def57996 (Make frontend and dashboard mobile responsive)
             <h1 className='mb-3 fs-2'>Free and open market education</h1>
             <p>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
             <a href='/' style={{textDecoration: "none"}}>Varsity<i class="fa fa-arrow-right-long"></i></a>
