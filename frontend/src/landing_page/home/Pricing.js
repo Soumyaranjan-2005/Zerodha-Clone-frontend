cc7a972130d542bdcaee3fcc92be6a8bad8f019d
Soumya Ranjan Mahanna <soumyaranjanmahanna5@gmail.com>
2026-06-26 13:28:48 +0530

    implement responsiveness 1

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -4,7 +4,7 @@ function Pricing() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-4">
+        <div className="col-12 col-lg-4 mb-5">
           <h1 className="mb-3 fs-2">Unbeatable pricing</h1>
           <p>
             We pioneered the concept of discount broking and price transparency
@@ -14,14 +14,14 @@ function Pricing() {
             See pricing<i class="fa fa-arrow-right-long"></i>
           </a>
         </div>
-        <div className="col-2"></div>
-        <div className="col-6 mb-5">
+        <div className="col-12 col-lg-2"></div>
+        <div className="col-12 col-lg-6 mb-5">
           <div className="row text-center">
-            <div className="col p-3 border">
+            <div className="col-12 col-md-6 p-3 border">
               <h1 className="mb-3">0</h1>
               <p>Free equity delivery and direct mutual funds</p>
             </div>
-            <div className="col p-3 border">
+            <div className="col-12 col-md-6 p-3 border">
               <h1 className="mb-3">20</h1>
               <p>Intraday and F&O</p>
             </div>
