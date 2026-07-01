795eda28b44a9a851e2dd782bed083a639a4fd3a
Soumya Ranjan Mahanna <soumyaranjanmahanna5@gmail.com>
2026-07-01 21:04:14 +0530

    fix: untrack local environment vars and fix dashboard unauthenticated redirect path

--- dashboard/src/config.js
+++ dashboard/src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://zerodha-clone-backend-yok4.onrender.com";
 export const FRONTEND_BASE_URL = process.env.REACT_APP_FRONTEND_BASE_URL || "https://srm-zerodha.vercel.app";
-export const FRONTEND_LOGIN_PATH = process.env.REACT_APP_FRONTEND_LOGIN_PATH || "/";
+export const FRONTEND_LOGIN_PATH = process.env.REACT_APP_FRONTEND_LOGIN_PATH || "/Login";
